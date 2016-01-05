a3fa7c8edb12f0e7baebb922e360db0e2f3c1506
Daksh Sharma <dashatrix@gmail.com>
2016-01-04 22:49:58 -0800

    CheckPoint 33 Complete: Blocly Project Started

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BlocActivity.java
@@ -0,0 +1,19 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by Daksh on 1/4/16.
+ */
+public class BlocActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+
+    }
+}
