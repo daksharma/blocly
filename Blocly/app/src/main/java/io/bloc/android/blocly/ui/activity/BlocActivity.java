c91d16462b2b8fe19954dc67c4ea457681e0b90d
Daksh Sharma <dashatrix@gmail.com>
2016-01-06 23:07:38 -0800

    Checkpoint 36 Complete: Blocly Make a List

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BlocActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BlocActivity.java
@@ -2,15 +2,22 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 
 /**
  * Created by Daksh on 1/4/16.
  */
 public class BlocActivity extends Activity {
+
+    private ItemAdapter itemAdapter;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -19,5 +26,12 @@ public class BlocActivity extends Activity {
         Toast.makeText(this,
                        BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
                        Toast.LENGTH_LONG).show();
+
+        itemAdapter = new ItemAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
     }
 }
