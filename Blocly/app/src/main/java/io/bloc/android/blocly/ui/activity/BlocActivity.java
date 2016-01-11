3763d5f41ef4cea3bca3b5a021e049e116778d70
Daksh Sharma <dashatrix@gmail.com>
2016-01-10 22:49:22 -0800

    CheckPoint 43 Complete: Blocly Drawer Layout

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BlocActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BlocActivity.java
@@ -1,10 +1,16 @@
 package io.bloc.android.blocly.ui.activity;
 
-import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
@@ -14,24 +20,59 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 /**
  * Created by Daksh on 1/4/16.
  */
-public class BlocActivity extends Activity {
+public class BlocActivity extends AppCompatActivity {
 
-    private ItemAdapter itemAdapter;
+    private ItemAdapter           itemAdapter;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout          drawerLayout;
 
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-        Toast.makeText(this,
-                       BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
+        Toast.makeText(this, BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
                        Toast.LENGTH_LONG).show();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
+        setSupportActionBar(toolbar);
 
         itemAdapter = new ItemAdapter();
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
+        RecyclerView recyclerView = ( RecyclerView ) findViewById(R.id.rv_activity_blocly);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(itemAdapter);
+
+
+        // #5
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = ( DrawerLayout ) findViewById(R.id.dl_activity_blocly);
+        // #6
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0, 0);
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    // #7a
+    @Override
+    protected void onPostCreate (Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    // #7b
+    @Override
+    public void onConfigurationChanged (Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    // #7c
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if ( drawerToggle.onOptionsItemSelected(item) ) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
