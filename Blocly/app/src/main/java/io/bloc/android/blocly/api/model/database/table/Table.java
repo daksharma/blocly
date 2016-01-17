5fdf0e03a428622789679b3f26457848f49745eb
Daksh Sharma <dashatrix@gmail.com>
2016-01-17 15:25:36 -0800

    CheckPoint 52 Complete: Blocly DataBase Part 1

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/model/database/table/Table.java
@@ -0,0 +1,20 @@
+package io.bloc.android.blocly.api.model.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Daksh on 1/17/16.
+ */
+
+
+public abstract class Table {
+
+    protected static final String COLUMN_ID = "id";
+
+    public abstract String getName ();
+
+    public abstract String getCreateStatement ();
+
+    public void onUpgrade (SQLiteDatabase writableDatabase, int oldVersion, int newVersion) {
+    }
+}
